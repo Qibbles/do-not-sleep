24549b4a105200d43fee88935b61539b592637d5
Gregory CHUA <coffeecup@gmail.com>
2020-06-30 16:16:14 +0800

    Changed mouse move to CTRL keypress

--- do-not-sleep.py
+++ do-not-sleep.py
@@ -1,9 +1,13 @@
 import pyautogui
 import time
 import sys
+# from datetime import datetime
 
-while(True):
-    time.sleep(60)
-    pyautogui.move(1,1)
-    pyautogui.move(-2,-2)
-    pyautogui.move(1,1)
+while True:
+    time.sleep(300)
+    pyautogui.press('ctrl')
+    # pyautogui.move(1,1)
+    # pyautogui.move(-2,-2)
+    # pyautogui.move(1,1)
+    # now = datetime.now()
+    # print("Moved @ ", now)
